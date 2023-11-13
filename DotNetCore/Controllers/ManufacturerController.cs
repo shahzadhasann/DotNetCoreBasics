2df257dd6734f9aaa523c016ccf1c8b24cbbe18a
nomaannn <nomaan123456@gmail.com>
2023-11-13 16:15:06 +0530

    add manufacturer page commit

---
+++ DotNetCore/Controllers/ManufacturerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore.Controllers
+{
+    public class ManufacturerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
